1c53a941c8d575f15937d4f254854403cd0f523a
somtsho <179671017+spx77-e59@users.noreply.github.com>
2024-10-07 19:46:56 +0600

    Fetch data

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("yoi");
+async function getData(location) {
+  const API_KEY = "9ZWJP8DXHVBVKJGL3WGLBZBLX";
+  try {
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=us&key=${API_KEY}&contentType=json`,
+      { mode: "cors" }
+    );
+    if (!response.ok) {
+      throw new Error(`Error fetching data: ${response.status}`);
+    }
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+getData("sonam phuntsho");
